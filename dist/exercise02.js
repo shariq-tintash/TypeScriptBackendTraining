a9733297aac5f2d355cd9604030d2af8d23a1a36
shariq-tintash <shariq.rashid@tintash.com>
2022-07-24 01:28:17 +0500

    exercise 2 done

---
+++ dist/exercise02.js
@@ -0,0 +1,26 @@
+export const persons = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+export function logPerson(user) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+persons.forEach(logPerson);
