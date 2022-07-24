f1287c5ab3c6c26fc6ca61d3d894b3d942935e40
shariq-tintash <shariq.rashid@tintash.com>
2022-07-24 16:24:11 +0500

    exercise 05 done

---
+++ dist/exercise05.js
@@ -0,0 +1,57 @@
+export const persons = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    {
+        type: 'admin',
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        type: 'user',
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        type: 'admin',
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    },
+    {
+        type: 'user',
+        name: 'Wilson',
+        age: 23,
+        occupation: 'Ball'
+    },
+    {
+        type: 'admin',
+        name: 'Agent Smith',
+        age: 23,
+        role: 'Administrator'
+    }
+];
+export const isAdmin = (person) => person.type === 'admin';
+export const isUser = (person) => person.type === 'user';
+export function logPerson(person) {
+    let additionalInformation = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+export function filterUsers(persons, criteria) {
+    return persons.filter(isUser).filter((user) => {
+        const criteriaKeys = Object.keys(criteria);
+        return criteriaKeys.every((fieldName) => {
+            return user[fieldName] === criteria[fieldName];
+        });
+    });
+}
+console.log('Users of age 23:');
+filterUsers(persons, {
+    age: 23
+}).forEach(logPerson);
