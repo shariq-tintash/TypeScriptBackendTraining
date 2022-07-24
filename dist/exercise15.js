4c1700d7fbf91d446dd0d2f8bef2eda96878408a
shariq-tintash <shariq.rashid@tintash.com>
2022-07-24 17:36:17 +0500

    exercise 15 done

---
+++ dist/exercise15.js
@@ -0,0 +1,19 @@
+export class ObjectManipulator {
+    constructor(obj) {
+        this.obj = obj;
+    }
+    set(key, value) {
+        return new ObjectManipulator(Object.assign(Object.assign({}, this.obj), { [key]: value }));
+    }
+    get(key) {
+        return this.obj[key];
+    }
+    delete(key) {
+        const newObj = Object.assign({}, this.obj);
+        delete newObj[key];
+        return new ObjectManipulator(newObj);
+    }
+    getObject() {
+        return this.obj;
+    }
+}
